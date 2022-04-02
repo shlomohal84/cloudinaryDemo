f3357ff82927b0b55758f0beca4ab05e6ac131b2
shlomohal <stairwaytoheaven84@gmail.com>
2022-04-02 19:03:55 +0300

    change env params

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 const { cloudinary } = require("./utils/cloudinary");
 const { dbConnect, ImageModel } = require("./config/db");
 dbConnect();
